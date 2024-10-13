83bb05d7a7de46a3a84724ee2f32a743d9b458fa
github-action <patrick.downey@dioadconsulting.com>
2024-10-14 00:00:48 +0100

    Brew formula update for connect version v0.60.3

--- Formula/connect.rb
+++ Formula/connect.rb
@@ -5,20 +5,20 @@
 class Connect < Formula
   desc "Dioad Connect is an SNI based TCP reverse tunnel that provides end-to-end encrypted traffic whereever you need it."
   homepage "https://dioad.com/"
-  version "0.60.2"
+  version "0.60.3"
   depends_on :macos
 
   on_intel do
-    url "https://releases.lab.dioad.net/data/connect/0.60.2/connect_darwin_amd64.zip", using: CurlDownloadStrategy
-    sha256 "362ff7e6067100e7985338c2eb417ad217f2b2397446bbf2a01529c7fae8b118"
+    url "https://releases.lab.dioad.net/data/connect/0.60.3/connect_darwin_amd64.zip", using: CurlDownloadStrategy
+    sha256 "b7f089964a4ec11a02f7cace1c338760e0c6b955cf3cfaa3c9a7b2f8509fd472"
 
     def install
       bin.install "connect"
     end
   end
   on_arm do
-    url "https://releases.lab.dioad.net/data/connect/0.60.2/connect_darwin_arm64.zip", using: CurlDownloadStrategy
-    sha256 "4c7101305076290ed4515b82f3997f3939ad3c0db33a1a4a2374975bccaa7bdf"
+    url "https://releases.lab.dioad.net/data/connect/0.60.3/connect_darwin_arm64.zip", using: CurlDownloadStrategy
+    sha256 "f3c0f0bcba572abead7443de0daf0488d735238d0b6e39def537fb4f75807d1a"
 
     def install
       bin.install "connect"
